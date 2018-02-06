95ca00fe59ce2b50ceb4c1efe8100988e26c6819
leehomlee <leehom_lzw@163.com>
2018-02-06 15:59:04 +0800

    add

---
+++ ADT.Core.Middleware/HelloWorldMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADT.Core.Middleware
+{
+    public class HelloWorldMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public HelloWorldMiddleware(RequestDelegate _next)
+        {
+            this.next = _next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello World!(Use in Class)\n");
+            await next(context);
+        }
+    }
+}
---
+++ ADT.Core.Middleware/Middleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADT.Core.Middleware
+{
+    public static class Middleware
+    {
+        public static void RunHelloWorld(this IApplicationBuilder app)
+        {
+            app.Run(async (context) => 
+            {
+                await context.Response.WriteAsync("Hello World! (Run)");
+            });
+        }
+
+        public static void UseHelloWorld(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) => 
+            {
+                await context.Response.WriteAsync("Hello World! (Use)\n");
+                await next();
+            });
+        }
+
+        public static IApplicationBuilder UseHelloWorldInClass(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HelloWorldMiddleware>();
+        }
+    }
+}
