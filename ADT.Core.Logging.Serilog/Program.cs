eea37fd1ecfbfcdd1fef0b9075649ed1e10e0d5c
leehomlee <leehom_lzw@163.com>
2018-02-28 13:42:37 +0800

    serilog

---
+++ ADT.Core.Logging.Serilog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace ADT.Core.Logging.Serilog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("ApiVersion", "1.2.5000")
+                .WriteTo.LiterateConsole()
+                //.WriteTo.Seq("http://localhost:48464")
+                .CreateLogger()
+                .ForContext<HelloWorldMiddleware2>();
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+    }
+}
---
+++ ADT.Core.Logging.Serilog/UseMiddlewareExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADT.Core.Logging.Serilog
+{
+    public static class UseMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHelloWorld(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HelloWorldMiddleware2>();
+        }
+    }
+
+    public class HelloWorldMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<HelloWorldMiddleware> logger;
+        public HelloWorldMiddleware(RequestDelegate _next, ILogger<HelloWorldMiddleware> _logger)
+        {
+            next = _next;
+            logger = _logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            this.logger.LogInformation(101, "Invoke executing...");
+            await context.Response.WriteAsync("Hello Logging!");
+            this.logger.LogInformation(201, "Invoke executed...");
+        }
+    }
+
+    //ILoggerFactory
+    public class HelloWorldMiddleware2
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger logger;
+        public HelloWorldMiddleware2(RequestDelegate _next, ILoggerFactory factory)
+        {
+            next = _next;
+            logger = factory.CreateLogger("HelloWorldMiddleware2");
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var message = new
+            {
+                GreetingTo = "James Bond",
+                GreetingTime = "Morning",
+                GreetingType = "Good"
+            };
+            this.logger.LogInformation("Inoke executing {@message}", message);
+
+
+            //this.logger.LogInformation(101, "Invoke2 executing...");
+            await context.Response.WriteAsync("Hello Logging2!");
+            //this.logger.LogInformation(201, "Invoke2 executed...");
+
+            this.logger.LogInformation(
+                "Inoke executed by {developer} at {time}", "lilei", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+}
