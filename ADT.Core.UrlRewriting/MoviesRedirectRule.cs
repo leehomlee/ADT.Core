9c305c2f5119b4a63a9fe5ae647e6da8cae2316c
leehomlee <leehom_lzw@163.com>
2018-03-01 14:23:51 +0800

    UrlRewrite

---
+++ ADT.Core.UrlRewriting/MoviesRedirectRule.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
+using System.Linq;
+
+namespace ADT.Core.UrlRewriting
+{
+    public class MoviesRedirectRule : IRule
+    {
+        private readonly string[] matchPaths;
+        private readonly PathString newPath;
+        private string HeaderName;
+
+        public MoviesRedirectRule(string[] _matchPaths, string _newPath)
+        {
+            matchPaths = _matchPaths;
+            newPath = _newPath;
+        }
+
+        public void ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            if (request.Path.StartsWithSegments(newPath))
+            {
+                return;
+            }
+
+            if (matchPaths.Contains(request.Path.Value))
+            {
+                var newLocation = $"{newPath}{request.QueryString}";
+                var response = request.HttpContext.Response;
+                response.StatusCode = StatusCodes.Status302Found;
+                context.Result = RuleResult.EndResponse;
+                response.Headers[HeaderNames.Location] = newLocation;
+            }
+        }
+    }
+}
