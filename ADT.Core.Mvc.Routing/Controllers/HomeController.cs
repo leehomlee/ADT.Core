9c17004cdc766305d77266d7fd73fc8c018e11ac
leehomlee <leehom_lzw@163.com>
2018-03-01 15:26:30 +0800

    mvc.routing

---
+++ ADT.Core.Mvc.Routing/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADT.Core.Mvc.Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home/Index");
+        }
+
+        public IActionResult PageOne()
+        {
+            return Content("Home/One");
+        }
+
+        [HttpGet]
+        public IActionResult PageTwo()
+        {
+            return Content("(GET) Home/Two");
+        }
+
+        [HttpPost]
+        public IActionResult PageTwo(int id)
+        {
+            return Content($"(Post) Home/Two: {id}");
+        }
+    }
+}
---
+++ ADT.Core.Mvc.Routing/Controllers/WorkController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADT.Core.Mvc.Routing.Controllers
+{
+    [Route("work")]
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Work/Index");
+        }
+
+        [Route("one")]
+        public IActionResult PageOne()
+        {
+            return Content("Work/One");
+        }
+
+        [HttpGet("two")]
+        public IActionResult PageTwo()
+        {
+            return Content("(GET) Work/Two");
+        }
+
+        [HttpPost("two/{id?}")]
+        public IActionResult PageTwo(int id)
+        {
+            return Content($"(Post) Work/Two: {id}");
+        }
+    }
+}
