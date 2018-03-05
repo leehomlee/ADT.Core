462ab565c558f4a4986f9e5eec5eea8e39ab60fb
leehomlee <leehom_lzw@163.com>
2018-03-05 18:30:55 +0800

    remote modelvalidation

---
+++ ADT.Core.Mvc.ModelValidation.Remote/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ADT.Core.Mvc.ModelValidation.Remote.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Models.Home.EmployeeInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var json = JsonConvert.SerializeObject(model);
+            return Content(json);
+        }
+
+        public IActionResult ValidateEmployeeNo(string EmployeeNo)
+        {
+            if (EmployeeNo == "007")
+            {
+                return Json(data: "007 is already assigned to James Bond!");
+            }
+            return Json(data: true);
+        }
+    }
+}
---
+++ ADT.Core.Mvc.ModelValidation.Remote/Models/Home/EmployeeInputModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADT.Core.Mvc.ModelValidation.Remote.Models.Home
+{
+    public class EmployeeInputModel
+    {
+        [Required]
+        [Remote(action: "ValidateEmployeeNo", controller: "Home")]
+        public string EmployeeNo { get; set; }
+    }
+}
