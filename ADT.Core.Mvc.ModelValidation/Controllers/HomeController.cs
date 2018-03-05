7ca5b20b593f4886d86cff4eaf06f47735c2a1ac
leehomlee <leehom_lzw@163.com>
2018-03-05 17:52:16 +0800

    modelvalidation

---
+++ ADT.Core.Mvc.ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ADT.Core.Mvc.ModelValidation.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADT.Core.Mvc.ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Model Validation!");
+        }
+
+        [HttpPost]
+        public IActionResult Save([FromBody]EmployeeInputModel model)
+        {
+            if (model.Id == 1)
+            {
+                ModelState.AddModelError("Id", "Id already exists");
+            }
+
+            if(ModelState.IsValid)
+                return Ok(model);
+            return BadRequest(ModelState);
+        }
+    }
+}
