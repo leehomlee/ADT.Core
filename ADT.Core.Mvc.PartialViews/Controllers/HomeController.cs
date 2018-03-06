cedced29fd8c966746b1aff7bcef0564a29f03f9
leehomlee <leehom_lzw@163.com>
2018-03-06 13:36:21 +0800

    partial views

---
+++ ADT.Core.Mvc.PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADT.Core.Mvc.PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Models.Home.EmployeeViewModel
+            {
+                Id = 1,
+                Firstname = "Lei",
+                Surname = "Li",
+                Address = new Models.Home.AddressViewModel
+                {
+                    Line1 = "Shanghai",
+                    Line2 = "PuDong",
+                    Line3 = "BiBoRoad"
+                }
+            };
+            return View(model);
+        }
+    }
+}
