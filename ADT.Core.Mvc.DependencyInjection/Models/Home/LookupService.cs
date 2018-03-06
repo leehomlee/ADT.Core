09579809e9c9ff63d4a8f904f157aa1e2c2e1423
leehomlee <leehom_lzw@163.com>
2018-03-06 11:25:26 +0800

    mvc dependency injection

---
+++ ADT.Core.Mvc.DependencyInjection/Models/Home/LookupService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADT.Core.Mvc.DependencyInjection.Models.Home
+{
+    public interface ILookupService
+    {
+        List<SelectListItem> Genres { get; }
+    }
+
+    public class LookupService : ILookupService
+    {
+        public List<SelectListItem> Genres
+        {
+            get
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{ Value="1",Text="one"},
+                    new SelectListItem{ Value="2",Text="two"},
+                    new SelectListItem{ Value="3",Text="three"}
+                };
+            }
+        }
+    }
+}
