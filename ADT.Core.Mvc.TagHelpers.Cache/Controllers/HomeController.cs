33df88e453529070a35bfbc1f83da64fc116187b
leehomlee <leehom_lzw@163.com>
2018-03-07 15:24:08 +0800

    tag helpers cache

---
+++ ADT.Core.Mvc.TagHelpers.Cache/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADT.Core.Mvc.TagHelpers.Cache.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
