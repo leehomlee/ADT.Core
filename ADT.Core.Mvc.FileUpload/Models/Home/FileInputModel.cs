dbd2e8c4f51353c3e3873805e8153fcfe7b5d661
leehomlee <leehom_lzw@163.com>
2018-03-08 16:42:30 +0800

    fileupload

---
+++ ADT.Core.Mvc.FileUpload/Models/Home/FileInputModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ADT.Core.Mvc.FileUpload.Models.Home
+{
+    public class FileInputModel
+    {
+        public IFormFile FileToUpload { get; set; }
+    }
+}
---
+++ ADT.Core.Mvc.FileUpload/Models/Home/IFormFileExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ADT.Core.Mvc.FileUpload.Models.Home
+{
+    public static class IFormFileExtensions
+    {
+        public static string GetFilename(this IFormFile file)
+        {
+            return ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.ToString().Trim('"');
+        }
+
+        public static async Task<MemoryStream> GetFileStream(this IFormFile file)
+        {
+            MemoryStream stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            return stream;
+        }
+
+        public static async Task<byte[]> GetFileArray(this IFormFile file)
+        {
+            MemoryStream stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            return stream.ToArray();
+        }
+    }
+}
